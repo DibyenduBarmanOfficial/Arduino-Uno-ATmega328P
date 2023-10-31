4c8d45ac7a84b7f9e932329917a6bbd97f793802
Dibyendu Barman <119417662+DibyenduBarmanOfficial@users.noreply.github.com>
2023-10-31 13:18:43 +0530

    Update 03_Project_PWM_Dimmer-LED.ino

--- Hands-On_Projects/03_Project_PWM_Dimmer-LED.ino
+++ Hands-On_Projects/03_Project_PWM_Dimmer-LED.ino
@@ -7,12 +7,12 @@ const int pwmPin = 9;
 unsigned int i = 0;
 
 void setup() {
-  // not using
+  pinMode(pwmPin, OUTPUT); // Optional
 
 }
 
 void loop() {
-  for(i = 0; i < 255; i++)
+  for(i = 0; i < 255; i++) // In an 8-bit system, there are 256 possible values (2^8)
   {
     analogWrite(pwmPin, i);
     delay(2); // delay 2 mili seconds
@@ -20,7 +20,7 @@ void loop() {
   
   delay(2000); // delay 2 seconds
   
-  for(i = 255; i > 0; i--)
+  for(i = 255; i > 0; i--) // In an 8-bit system, there are 256 possible values (2^8)
   {
     analogWrite(pwmPin, i);
     delay(2); // delay 2 mili seconds
